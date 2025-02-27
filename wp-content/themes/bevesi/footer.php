02e8b17c9cb0e7fc7bb9d9fe3737719f0a4af93c
Willian Batista <willian.domingos@mec.gov.br>
2025-02-27 17:40:28 -0300

    alt

--- wp-content/themes/bevesi/footer.php
+++ wp-content/themes/bevesi/footer.php
@@ -67,6 +67,9 @@ jQuery( document ).ready(function( $ ) {
         $("#afreg_additionalshowhide_3234").css("display", "none");
         $("#afreg_additionalshowhide_3216").css("display", "none");
         $("#afreg_additionalshowhide_3217").css("display", "none");
+        $("#afreg_additional_3234").css("display", "none");
+        $("#afreg_additional_3216").css("display", "none");
+        $("#afreg_additional_3217").css("display", "none");
             
         if(valor=='Linkedin'){
             $("#afreg_additionalshowhide_3234").css("display", "block");
@@ -75,11 +78,11 @@ jQuery( document ).ready(function( $ ) {
         }
         if(valor=='Facebook'){
             $("#afreg_additionalshowhide_3217").css("display", "block");
-            $("#afreg_additional_3217").css("display", "none");
+            $("#afreg_additional_3217").css("display", "block");
             
         }
         if(valor=='Instagram'){
-            $("#afreg_additional_3216").css("display", "none");
+            $("#afreg_additional_3216").css("display", "block");
             $("#afreg_additionalshowhide_3216").css("display", "block");
         }
 
@@ -91,7 +94,7 @@ jQuery( document ).ready(function( $ ) {
 <style>
 
 input[type=checkbox], .woocommerce-form__input-checkbox{
-    background-color: #e5e7eb;
+    background-color: #aaa;
 }
 .header-type4  .site-header-custom-button>a{ 
 min-width: 209px;
