3de5776ad26ee50f7437659e7e2ff3b1a0e7ac66
Willian Batista <willian.domingos@mec.gov.br>
2025-04-03 15:26:41 -0300

    alt

--- wp-content/themes/bevesi/footer.php
+++ wp-content/themes/bevesi/footer.php
@@ -1972,7 +1972,7 @@ window.onload = function() {
 
         <?php 
             if (strpos($_SERVER['HTTP_REFERER'], '?etapa_cadastro=3') !== false) { ?>
-                window.location('/lista_planos');
+                window.location=('/lista_planos');
         <?php }  ?>
 
     setInterval(function() { limpa_campos_etapa(2); }, 500);
