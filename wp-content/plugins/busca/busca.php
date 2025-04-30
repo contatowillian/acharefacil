f681e25b3c015b711055f518ff67e1d485cb8eb9
Willian Batista <willian.domingos@mec.gov.br>
2025-04-30 15:52:52 -0300

    alt

--- wp-content/plugins/busca/busca.php
+++ wp-content/plugins/busca/busca.php
@@ -72,12 +72,12 @@ function content_buscaUsuariosAnunciantes($content) {
         }
 
         $consulta_usuarios_anunciantes = "SELECT DISTINCT
-        us.ID,
-        us.user_login
-        FROM wp_users AS us
-        JOIN wp_usermeta AS nome_do_seu_negocio  ON  us.ID = nome_do_seu_negocio.user_id  AND nome_do_seu_negocio.meta_key = 'afreg_additional_3288'
-         and nome_do_seu_negocio.meta_value ='sim'
-        where us.user_status = 0 $filtro_extra rand() limit 3 union ";
+                                          us.ID,
+                                          us.user_login
+                                          FROM wp_users AS us
+                                          JOIN wp_usermeta AS nome_do_seu_negocio  ON  us.ID = nome_do_seu_negocio.user_id  AND nome_do_seu_negocio.meta_key = 'afreg_additional_3288'
+                                          and nome_do_seu_negocio.meta_value ='sim'
+                                          where us.user_status = 0 $filtro_extra order by rand() limit 3 union ";
 
         $consulta_usuarios_anunciantes = "SELECT DISTINCT
                                           us.ID,
