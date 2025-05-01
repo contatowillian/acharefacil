04a82f133e2251d00de87c00c6d582812cdfeeb6
Willian Batista <willian.domingos@mec.gov.br>
2025-05-01 14:25:36 -0300

    Alteração no detalhe do imovel

--- wp-content/themes/bevesi/footer.php
+++ wp-content/themes/bevesi/footer.php
@@ -188,11 +188,8 @@ margin-top: -20px;
 
 
 .page-id-3290 .anuncios_semelhantes_bloco .product-content-footer{
-    font-size: 0.6875rem;
-    font-weight: 700;
-    text-transform: uppercase;
-    text-decoration: none;
-    color: var(--color-slate-600);
+    font-size: 13px;
+    color: #575252;
 }
 .page-id-3290 .single-ajax{
     display: none !important;
