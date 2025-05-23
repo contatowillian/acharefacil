92d33b8fa8d1bff23abd5dcf4656c10b7be86d96
Willian Batista <willian.domingos@mec.gov.br>
2025-05-23 12:29:04 -0300

    alteracao

--- wp-content/plugins/carrosel_anunciantes/carrosel_anunciantes.php
+++ wp-content/plugins/carrosel_anunciantes/carrosel_anunciantes.php
@@ -36,6 +36,7 @@ function content_buscaCarroselAnunciantes($content) {
 
 
       $users_anunciantes_carrosel = $wpdb->get_results($consulta_anunciantes_carrosel);
+      $textotitulocat = 'Veja nosso destaques';
         
        ob_start();
        include('tpl/CarroselAnunciantes.phtml');
@@ -52,3 +53,51 @@ function content_buscaCarroselAnunciantes($content) {
 
 
 add_filter('the_content', 'content_buscaCarroselAnunciantes');
+
+
+
+
+function content_buscaCarroselAnunciantesRecentes($content) {
+  global $grupo,
+         $wpdb;
+
+   if (is_front_page()) {
+
+  //  echo "yyy";
+  //  exit;
+
+    
+    $consulta_anunciantes_carrosel = "SELECT DISTINCT
+    us.ID,
+    us.user_login,
+    nome_do_seu_negocio.meta_value as nome_do_seu_negocio,
+    descricao.meta_value  as descricao,
+    foto_do_anunciante.meta_value  as foto_do_anunciante
+    FROM wp_users AS us
+    JOIN wp_usermeta AS afreg_new_user_status  ON  us.ID = afreg_new_user_status.user_id  AND afreg_new_user_status.meta_key = 'afreg_new_user_status' and afreg_new_user_status.meta_value ='approved'
+    JOIN wp_usermeta AS categoria  ON  us.ID = categoria.user_id  AND categoria.meta_key = 'afreg_additional_3213'
+    JOIN wp_usermeta AS nome_do_seu_negocio  ON  us.ID = nome_do_seu_negocio.user_id  AND nome_do_seu_negocio.meta_key = 'afreg_additional_3224'
+    JOIN wp_usermeta AS descricao  ON  us.ID = descricao.user_id  AND descricao.meta_key = 'afreg_additional_3226'
+    JOIN wp_usermeta AS foto_do_anunciante  ON  us.ID = foto_do_anunciante.user_id  AND foto_do_anunciante.meta_key = 'afreg_additional_3212'
+    where us.user_status = 0  order by user_registered desc limit 7 ";
+  /*  JOIN wp_usermeta AS destaque  ON  us.ID = destaque.user_id  AND destaque.meta_key = 'afreg_additional_3288' AND destaque.meta_value = 'sim'*/
+
+
+    $users_anunciantes_carrosel = $wpdb->get_results($consulta_anunciantes_carrosel);
+    $textotitulocat = 'Veja nosso anúncios recentes';
+
+     ob_start();
+     include('tpl/CarroselAnunciantes.phtml');
+     $template = ob_get_clean();
+
+     $content = str_replace('[[carrosel_anunciantes_recentes]]', $template, $content);
+  }
+  
+
+  return $content;
+}
+
+
+
+
+add_filter('the_content', 'content_buscaCarroselAnunciantesRecentes');
