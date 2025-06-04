fdb412f000592b30b849602cba1422aa45fc2261
Willian Batista <willian.domingos@mec.gov.br>
2025-06-04 18:26:22 -0300

    alt

--- wp-content/plugins/dokan-lite/templates/account/become-a-vendor-section.php
+++ wp-content/plugins/dokan-lite/templates/account/become-a-vendor-section.php
@@ -1,4 +1,4 @@
-<?php header("location: /my-account/edit-account/?etapa_cadastro=2&opcao_deslogar=1");
+<?php header("location: /my-account/edit-account");
 
 /**
  * Dokan Become A Vendor Section Template.
