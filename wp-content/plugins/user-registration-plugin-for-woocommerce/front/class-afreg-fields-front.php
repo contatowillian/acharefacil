aa1a6ae7b05f636f99582fe2ef0dcb4f115c4b74
Willian Batista <willian.domingos@mec.gov.br>
2025-06-17 12:15:58 -0300

    alt

--- wp-content/plugins/user-registration-plugin-for-woocommerce/front/class-afreg-fields-front.php
+++ wp-content/plugins/user-registration-plugin-for-woocommerce/front/class-afreg-fields-front.php
@@ -3505,8 +3505,7 @@ if ( !class_exists( 'Addify_Registration_Fields_Addon_Front' ) ) {
 
 									$target_path = $_SERVER['DOCUMENT_ROOT'].'/wp-content/uploads/addify_registration_uploads/';
 
-									echo __DIR__.'/wp-content/uploads/addify_registration_uploads/';
-									exit;
+								
 
 									if(!file_put_contents(__DIR__.'/teste.txt' , 'hello')){
 										echo "erro no file_put_contents";    
