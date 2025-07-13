a39b2df3e33c96c5db2c332393af8d599d07dd57
Willian Batista <willian.domingos@mec.gov.br>
2025-07-13 14:14:30 -0300

    alt

--- wp-content/plugins/carrosel_anunciantes/carrosel_anunciantes.php
+++ wp-content/plugins/carrosel_anunciantes/carrosel_anunciantes.php
@@ -24,7 +24,8 @@ function content_buscaCarroselAnunciantes($content) {
       }
       if(isset($_GET['teste_cachorro'])){
             
-           buscar_usuarios_por_meta_like('afreg_additional_3226', '%festa%');
+
+           buscar_usuarios_por_meta_like('afreg_additional_3226', '%Eventos%');
             exit;
       }
     
