66699b7aa2bf2ab9844e5d0902d14e45783e5244
Willian Batista <willian.domingos@mec.gov.br>
2025-07-14 18:11:57 -0300

    alt

--- wp-content/plugins/carrosel_anunciantes/carrosel_anunciantes.php
+++ wp-content/plugins/carrosel_anunciantes/carrosel_anunciantes.php
@@ -94,7 +94,7 @@ function buscar_usuarios_por_meta_like_get_users($meta_key, $search_term) {
         array(
           'compare_key' => 'LIKE',
           'key'     => 'afreg_additional_',
-          'value'   => 'cachorro',
+          'value'   => 'adestrador de cachorro',
           'compare' => 'like'
         )
     )
