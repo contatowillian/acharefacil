3c7d1abfce6eb8cdd205762b916a59d3c67e5141
Willian Batista <willian.domingos@mec.gov.br>
2025-07-20 20:22:05 -0300

    alt

---
+++ alteraVariaveisGetBusca.php
@@ -0,0 +1,8 @@
+<?php
+
+ if(isset($_GET['s'])){
+
+    $_GET['s'] = str_replace("cachorro","cão",strtolower($_GET['s']));
+    $_GET['s'] = str_replace("cachorros","cães",strtolower($_GET['s']));
+ 
+}
