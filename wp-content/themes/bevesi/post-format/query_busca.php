8eb7c6c4503c11105c066cd325ab296d0beac3f9
Willian Batista <willian.domingos@mec.gov.br>
2025-07-19 23:34:16 -0300

    alt

--- wp-content/themes/bevesi/post-format/query_busca.php
+++ wp-content/themes/bevesi/post-format/query_busca.php
@@ -1,7 +1,11 @@
 <?php
 
 echo 'author_meta'.the_author_meta('ID');
+echo 'author_meta2'.$post->post_author;
+$value= 0 ;
 
-//exit;
+if(!in_array($value, $user_id_post, true)){
+    array_push($user_id_post, $value);
+}
 
 ?>
