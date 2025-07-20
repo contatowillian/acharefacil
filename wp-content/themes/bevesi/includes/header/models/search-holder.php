23bdb8dc7ff809588b20b56050f69224fbbf3d4e
Willian Batista <willian.domingos@mec.gov.br>
2025-07-20 00:04:48 -0300

    alt

--- wp-content/themes/bevesi/includes/header/models/search-holder.php
+++ wp-content/themes/bevesi/includes/header/models/search-holder.php
@@ -8,11 +8,11 @@ if ( ! function_exists( 'bevesi_search_holder' ) ) {
 			<div id="search-drawer" class="site-drawer site-search-drawer get-header-height">
 				<div class="site-drawer-inner get-mobile-nav-height">
 					<div class="site-header-row site-drawer-body">
-						<form action="<?php echo esc_url( home_url( '/busca'  ) ); ?>" class="search-form" role="search" method="get">
+						<form action="<?php echo esc_url( home_url( '/'  ) ); ?>" class="search-form" role="search" method="get">
 							<div class="search-form-icon">
 								<i class="klb-icon-search"></i>
 							</div><!-- search-form-icon -->
-							<input type="search" value="" class="form-control search-input size-lg" name="palavra_chave" placeholder="Procure os melhores serviços próximos à você" autocomplete="off">
+							<input type="search" value="" class="form-control search-input size-lg" name="s" placeholder="Procure os melhores serviços próximos à você" autocomplete="off">
 							<input type="hidden" class='us_pos' name="us_pos">
 						</form>
 					</div><!-- site-header-row -->
