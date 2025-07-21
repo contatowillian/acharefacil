2fd673e93532611ec250a159c471637cb28e4560
Willian Batista <willian.domingos@mec.gov.br>
2025-07-21 10:05:07 -0300

    alt

--- wp-includes/functions.php
+++ wp-includes/functions.php
@@ -9043,10 +9043,10 @@ function set_posts_per_page( $query ) {
 
 
   if ( ( ! is_admin() ) && ( $query === $wp_the_query ) && ( $query->is_search() ) ) {
-    $query->set( 'posts_per_page', 50 );
+    $query->set( 'posts_per_page', 52 );
   }
   elseif ( ( ! is_admin() ) && ( $query === $wp_the_query ) && ( $query->is_archive() ) ) {
-    $query->set( 'posts_per_page', 50 );
+    $query->set( 'posts_per_page', 52 );
   }
   // Etc..
 
