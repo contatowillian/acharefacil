b1fe6e35fd8cc29636a0bae01e85cae11d00e03e
Willian Batista <willian.domingos@mec.gov.br>
2025-09-23 18:35:40 -0300

    alt

--- wp-content/plugins/carrosel_anunciantes/carrosel_anunciantes.php
+++ wp-content/plugins/carrosel_anunciantes/carrosel_anunciantes.php
@@ -15,19 +15,6 @@ function content_buscaCarroselAnunciantes($content) {
 
      if (is_front_page()) {
 
-      if(isset($_GET['teste_caes'])){
-        $user_query = new WP_User_Query( array( 'search' => 'adestrador de cães' ) );
-        echo "<pre>";
-        print_r($user_query );
-        echo "</pre>";
-        exit;
-      }
-      if(isset($_GET['teste_cachorro'])){
-
-        buscar_usuarios_por_meta_like_get_users('afreg_additional_', 'cachorro');
-        exit;
-      }
-    
 
 
 
@@ -51,10 +38,6 @@ function content_buscaCarroselAnunciantes($content) {
       where us.user_status = 0   and  foto_do_anunciante.meta_value!='' and  foto_do_anunciante.meta_value!='00000000-0000-0000-0000-000000000000'  order by rand() limit 15 ";
     /*  JOIN wp_usermeta AS destaque  ON  us.ID = destaque.user_id  AND destaque.meta_key = 'afreg_additional_3288' AND destaque.meta_value = 'sim'*/
 
-
-  
-
-
        $users_anunciantes_carrosel = $wpdb->get_results($consulta_anunciantes_carrosel);
        $textotitulocat = 'Veja nosso destaques';
        $nome_carrosel = 'site-slider-anunciantes1';
@@ -73,74 +56,6 @@ function content_buscaCarroselAnunciantes($content) {
 
 
 
-function buscar_usuarios_por_meta_like_get_users($meta_key, $search_term) {
-  
-  // Exemplo de como obter o termo de busca (se vier de um formulário, etc.)
-  $_GET['user_search'] = 'cachorro';
-
-  $search_term = isset($_GET['user_search']) ? sanitize_text_field($_GET['user_search']) : ''; 
-  
-  // Ou um termo fixo para teste:
-  // $search_term = 'termo de teste';
-  
-  if ( ! empty( $search_term ) ) {
-      $args = array(
-          'search'         => '*' . esc_attr( $search_term ) . '*', // Relevanssi usa este para buscar nos campos indexados
-          'number'         => 10, 
-          'paged'          => ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1,
-      );
-  
-      // O filtro é essencial para que o Relevanssi intercepte a query
-      add_filter( 'relevanssi_user_query_args', 'my_relevanssi_user_query_args' );
-  
-      $user_query = new WP_User_Query( $args );
-  
-      remove_filter( 'relevanssi_user_query_args', 'my_relevanssi_user_query_args' );
-  
-      function my_relevanssi_user_query_args( $query_args ) {
-          // Esta função pode ser usada para depuração ou para adicionar lógica extra.
-          // Por padrão, se o Relevanssi estiver configurado para indexar usuários e o campo,
-          // ele já deve interceptar a busca com base no parâmetro 'search'.
-          return $query_args;
-      }
-  
-      // ... (restante do código para exibir resultados e paginação)
-      if ( ! empty( $user_query->get_results() ) ) {
-          echo '<h2>Resultados da busca por usuários para "' . esc_html( $search_term ) . '"</h2>';
-          echo '<ul>';
-          foreach ( $user_query->get_results() as $user ) {
-              echo '<li>';
-              echo '<a href="' . esc_url( get_author_posts_url( $user->ID ) ) . '">' . esc_html( $user->display_name ) . '</a>';
-              $afreg_value = get_user_meta( $user->ID, 'afreg_additional', true );
-              if ( ! empty( $afreg_value ) ) {
-                  echo ' (Campo Adicional: ' . esc_html( $afreg_value ) . ')';
-              }
-              echo '</li>';
-          }
-          echo '</ul>';
-          // Paginação
-          $total_users    = $user_query->total_users;
-          $users_per_page = $args['number'];
-          $total_pages    = ceil( $total_users / $users_per_page );
-  
-          if ( $total_pages > 1 ) {
-              echo '<div class="pagination">';
-              for ( $i = 1; $i <= $total_pages; $i++ ) {
-                  echo '<a href="' . add_query_arg( 'paged', $i, get_permalink() ) . '">' . $i . '</a> ';
-              }
-              echo '</div>';
-          }
-      } else {
-          echo '<p>Nenhum usuário encontrado para "' . esc_html( $search_term ) . '".</p>';
-      }
-  
-  } else {
-      echo '<p>Por favor, digite um termo de busca para usuários.</p>';
-  }
-  
-
-}
-
 
 
 add_filter('the_content', 'content_buscaCarroselAnunciantes');
@@ -158,7 +73,7 @@ function content_buscaCarroselAnunciantesRecentes($content) {
   //  exit;
 
     
-    $consulta_anunciantes_carrosel = "SELECT DISTINCT
+    $consulta_anunciantes_carrosel = "SELECT 
     us.ID,
     us.user_login,
     nome_do_seu_negocio.meta_value as nome_do_seu_negocio,
@@ -171,7 +86,7 @@ function content_buscaCarroselAnunciantesRecentes($content) {
     JOIN wp_usermeta AS nome_do_seu_negocio  ON  us.ID = nome_do_seu_negocio.user_id  AND nome_do_seu_negocio.meta_key = 'afreg_additional_3224'
     JOIN wp_usermeta AS descricao  ON  us.ID = descricao.user_id  AND descricao.meta_key = 'afreg_additional_3226'
     JOIN wp_usermeta AS foto_do_anunciante  ON  us.ID = foto_do_anunciante.user_id  AND foto_do_anunciante.meta_key = 'afreg_additional_3212'
-    where us.user_status = 0 and  foto_do_anunciante.meta_value!='' and  foto_do_anunciante.meta_value!='00000000-0000-0000-0000-000000000000'  order by user_registered desc limit 15 ";
+    where us.user_status = 0 and  foto_do_anunciante.meta_value!='' and  foto_do_anunciante.meta_value!='00000000-0000-0000-0000-000000000000'  order by us.user_registered  desc limit 15 ";
   /*  JOIN wp_usermeta AS destaque  ON  us.ID = destaque.user_id  AND destaque.meta_key = 'afreg_additional_3288' AND destaque.meta_value = 'sim'*/
 
 
