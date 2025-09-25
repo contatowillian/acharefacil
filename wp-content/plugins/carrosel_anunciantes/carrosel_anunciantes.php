81ca22ed658181452584f0c48d79c9e3ae66efe2
Willian Batista <willian.domingos@mec.gov.br>
2025-09-25 10:21:26 -0300

    alt

--- wp-content/plugins/carrosel_anunciantes/carrosel_anunciantes.php
+++ wp-content/plugins/carrosel_anunciantes/carrosel_anunciantes.php
@@ -46,6 +46,45 @@ function content_buscaCarroselAnunciantes($content) {
        $template = ob_get_clean();
 
        $content = str_replace('[[carrosel_anunciantes]]', $template, $content);
+
+
+
+        //  echo "yyy";
+        //  exit;
+
+          
+          $consulta_anunciantes_carrosel = "SELECT 
+          us.ID,
+          us.user_login,
+          nome_do_seu_negocio.meta_value as nome_do_seu_negocio,
+          descricao.meta_value  as descricao,
+          foto_do_anunciante.meta_value  as foto_do_anunciante
+          FROM wp_users AS us
+          JOIN wp_usermeta AS afreg_new_user_status  ON  us.ID = afreg_new_user_status.user_id  AND afreg_new_user_status.meta_key = 'afreg_new_user_status' and afreg_new_user_status.meta_value ='approved'
+          JOIN wp_usermeta AS nome_do_seu_negocio  ON  us.ID = nome_do_seu_negocio.user_id  AND nome_do_seu_negocio.meta_key = 'afreg_additional_3224'
+          JOIN wp_usermeta AS descricao  ON  us.ID = descricao.user_id  AND descricao.meta_key = 'afreg_additional_3226'
+          JOIN wp_usermeta AS foto_do_anunciante  ON  us.ID = foto_do_anunciante.user_id  AND foto_do_anunciante.meta_key = 'afreg_additional_3212'
+          where us.user_status = 0 and  foto_do_anunciante.meta_value!='' and  foto_do_anunciante.meta_value!='00000000-0000-0000-0000-000000000000'  order by us.user_registered  desc limit 15 ";
+        /*  JOIN wp_usermeta AS destaque  ON  us.ID = destaque.user_id  AND destaque.meta_key = 'afreg_additional_3288' AND destaque.meta_value = 'sim'*/
+
+
+        if($_SERVER["REMOTE_ADDR"]=='187.22.179.112'){
+        //  echo $consulta_anunciantes_carrosel;
+      //  exit;
+      }
+
+
+
+    $users_anunciantes_carrosel = $wpdb->get_results($consulta_anunciantes_carrosel);
+    $textotitulocat = 'Veja nosso anúncios recentes';
+    $nome_carrosel = 'site-slider-anunciantes1';
+    $classe_carrosel = 'carrosel_recentes_lista';
+
+     ob_start();
+     include('tpl/CarroselAnunciantes.phtml');
+     $template = ob_get_clean();
+
+     $content = str_replace('[[carrosel_anunciantes_recentes]]', $template, $content);
     }
     
 
@@ -112,4 +151,4 @@ function content_buscaCarroselAnunciantesRecentes($content) {
 
 
 
-add_filter('the_content', 'content_buscaCarroselAnunciantesRecentes');
+//add_filter('the_content', 'content_buscaCarroselAnunciantesRecentes');
